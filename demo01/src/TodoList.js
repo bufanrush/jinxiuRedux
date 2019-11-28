a331b11ce8c6c7dcb6d03beddf65ebd9ee065f7f
bufan <l>
2019-11-29 01:16:13 +0800

    01

---
+++ demo01/src/TodoList.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import 'antd/dist/antd.css';
+import {Input,Button,List } from 'antd';
+import { CHANGE_INPUT , ADD_ITEM , DELETE_ITEM } from './store/actionTypes';
+import store from './store';
+
+class TodoList extends Component {
+constructor(props){
+    super(props)
+    this.state = store.getState();
+    this.changeInputValue = this.changeInputValue.bind(this);
+    this.clickBtn = this.clickBtn.bind(this);
+    this.storeChange = this.storeChange.bind(this);
+    //订阅
+    store.subscribe(this.storeChange);
+}
+    render() { 
+        return ( 
+            <div style={{margin:'10px'}}>
+                <div>
+                    <Input 
+                    placeholder={this.state.inputValue}
+                    style={{width:'250px',marginRight:'10px'}}
+                    onChange={this.changeInputValue}
+                    value={this.state.inputValue}
+                    />
+                    <Button 
+                    type="primary" 
+                    onClick={this.clickBtn}
+                    >增加</Button>
+                </div>
+                <div style={{margin:'10px',width:'300px'}}>
+                    <List 
+                        bordered
+                        dataSource={this.state.list}
+                        renderItem={(item,index)=>(<List.Item 
+                            onClick={this.deleteItem.bind(this,index)}
+                            >{item}</List.Item>)}
+                    />
+                </div>
+            </div>
+         );
+    }
+
+    storeChange(){
+        this.setState(store.getState());
+    }
+
+    changeInputValue(e){
+        const action = {
+            type:CHANGE_INPUT,
+            value:e.target.value
+        };
+        store.dispatch(action);        
+    }
+
+    clickBtn(){
+       const action = {type:ADD_ITEM};
+       store.dispatch(action);
+    }
+
+    deleteItem(index){
+        const action = {
+            type:DELETE_ITEM,
+            index
+        };
+        store.dispatch(action);
+    }
+
+}
+ 
+export default TodoList;
